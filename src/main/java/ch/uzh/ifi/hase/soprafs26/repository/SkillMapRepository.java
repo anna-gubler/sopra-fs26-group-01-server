4ea63ed6515d3f845fdf44df27d199772eabf36f
anna-gubler <agubler@quickline.ch>
2026-03-25 13:45:53 +0100

    feat(#35): create SkillMap Repository file

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/repository/SkillMapRepository.java
@@ -0,0 +1,14 @@
+package ch.uzh.ifi.hase.soprafs26.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ch.uzh.ifi.hase.soprafs26.entity.SkillMap;
+
+
+@Repository
+public interface SkillMapRepository extends JpaRepository<SkillMap,Long> {
+        List<SkillMap> findByOwnerId(Long ownerId);
+}
