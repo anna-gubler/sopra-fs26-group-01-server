7c6565c432df9d9d520e51ec4d17af12f137b11a
sebdahub <sebastian.huber2@uzh.ch>
2026-03-26 15:55:42 +0100

    test(#27) implemented UserServiceIntegrationTests and DTOMapperTests

--- src/test/java/ch/uzh/ifi/hase/soprafs26/rest/mapper/DTOMapperTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs26/rest/mapper/DTOMapperTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.Test;
 import ch.uzh.ifi.hase.soprafs26.constant.UserStatus;
 import ch.uzh.ifi.hase.soprafs26.entity.User;
 import ch.uzh.ifi.hase.soprafs26.rest.dto.UserGetDTO;
+import ch.uzh.ifi.hase.soprafs26.rest.dto.UserPatchDTO;
 import ch.uzh.ifi.hase.soprafs26.rest.dto.UserPostDTO;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,48 +24,60 @@ public class DTOMapperTest {
 	private static final String USERNAME = "firstname@lastname";
 	private static final String BIO = "Hey there! I'm using Whatsapp.";
 	private static final String TOKEN = "6dd696b4-83a2-42a6-8769-e2d755c6b8b8";
-
-	// create new UserPostDTO instance
-	private static UserPostDTO newUserPostDTO() {
-		UserPostDTO userPostDTO = new UserPostDTO();
-		userPostDTO.setUsername(USERNAME);
-		userPostDTO.setBio(BIO);
-		return userPostDTO;
-	}
-
-	// create new User instance
-	private static User newUser() {
-		User user = new User();
-		user.setUsername(USERNAME);
-		user.setBio(BIO);
-		user.setToken(TOKEN);
-		user.setStatus(UserStatus.OFFLINE);
-		return user;
-	}
+	private static final String PASSWORD = "somePassword123";
+	private static final LocalDateTime CREATION_DATE = LocalDateTime.of(2026, 1, 1, 0, 0);
 
 	@Test
 	public void shouldConvertUserPostDTOToEntity() {
-		UserPostDTO userPostDTO = newUserPostDTO();
+		UserPostDTO userPostDTO = new UserPostDTO();
+		userPostDTO.setUsername(USERNAME);
+		userPostDTO.setBio(BIO);
+		userPostDTO.setPassword(PASSWORD);
 
 		User user = DTOMapper.INSTANCE.convertUserPostDTOtoEntity(userPostDTO);
 
 		assertAll(
 				() -> assertEquals(userPostDTO.getUsername(), user.getUsername()),
-				() -> assertEquals(userPostDTO.getBio(), user.getBio()));
+				() -> assertEquals(userPostDTO.getBio(), user.getBio()),
+				() -> assertEquals(userPostDTO.getPassword(), user.getPassword()));
 
 	}
 
 	@Test
 	public void shouldConvertEntityToUserGetDTO() {
-		User user = newUser();
+		User user = new User();
 		user.setId(1L);
+		user.setUsername(USERNAME);
+		user.setBio(BIO);
+		user.setToken(TOKEN);
+		user.setStatus(UserStatus.OFFLINE);
+		user.setCreationDate(CREATION_DATE);
 
 		UserGetDTO userGetDTO = DTOMapper.INSTANCE.convertEntityToUserGetDTO(user);
 
 		assertAll(
 				() -> assertEquals(user.getId(), userGetDTO.getId()),
 				() -> assertEquals(user.getUsername(), userGetDTO.getUsername()),
-				() -> assertEquals(user.getStatus(), userGetDTO.getStatus()));
+				() -> assertEquals(user.getStatus(), userGetDTO.getStatus()),
+				() -> assertEquals(user.getBio(), userGetDTO.getBio()),
+				() -> assertEquals(user.getCreationDate(), userGetDTO.getCreationDate()));
 
+	}
+
+	@Test
+	public void shouldConvertUserPatchDTOToEntity() {
+		UserPatchDTO userPatchDTO = new UserPatchDTO();
+		userPatchDTO.setUsername(USERNAME);
+		userPatchDTO.setBio(BIO);
+		userPatchDTO.setPassword(PASSWORD);
+		userPatchDTO.setToken(TOKEN);
+
+		User user = DTOMapper.INSTANCE.convertUserPatchDTOtoEntity(userPatchDTO);
+
+		assertAll(
+				() -> assertEquals(userPatchDTO.getUsername(), user.getUsername()),
+				() -> assertEquals(userPatchDTO.getBio(), user.getBio()),
+				() -> assertEquals(userPatchDTO.getPassword(), user.getPassword()),
+				() -> assertEquals(userPatchDTO.getToken(), user.getToken()));
 	}
 }
