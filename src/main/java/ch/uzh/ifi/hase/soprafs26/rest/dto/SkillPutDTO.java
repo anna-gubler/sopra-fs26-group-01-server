fd4ef85f32c9eed3814231e8a150990c0fdf46b5
anna-gubler <agubler@quickline.ch>
2026-03-27 17:32:14 +0100

    feat(#54): create Put DTO

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/rest/dto/SkillPutDTO.java
@@ -0,0 +1,32 @@
+package ch.uzh.ifi.hase.soprafs26.rest.dto;
+
+public class SkillPutDTO {
+    private String name;
+    private String description;
+    private String resources;
+    private String difficulty;
+    private Integer level;
+    private Float positionX;
+    private Float positionY;
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public String getResources() { return resources; }
+    public void setResources(String resources) { this.resources = resources; }
+
+    public String getDifficulty() { return difficulty; }
+    public void setDifficulty(String difficulty) { this.difficulty = difficulty; }
+
+    public Integer getLevel() { return level; }
+    public void setLevel(Integer level) { this.level = level; }
+
+    public Float getPositionX() { return positionX; }
+    public void setPositionX(Float positionX) { this.positionX = positionX; }
+
+    public Float getPositionY() { return positionY; }
+    public void setPositionY(Float positionY) { this.positionY = positionY; }
+}
