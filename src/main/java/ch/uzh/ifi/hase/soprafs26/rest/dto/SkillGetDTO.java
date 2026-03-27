9fc1d36254cdc5aa0323aa6c727f248d0d22ee36
anna-gubler <agubler@quickline.ch>
2026-03-27 17:32:33 +0100

    feat(#55): create Get DTO

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/rest/dto/SkillGetDTO.java
@@ -0,0 +1,44 @@
+package ch.uzh.ifi.hase.soprafs26.rest.dto;
+
+public class SkillGetDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private String resources;
+    private String difficulty;
+    private Integer level;
+    private Float positionX;
+    private Float positionY;
+    private Boolean isLocked;
+    private Long mapId;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public String getResources() { return resources; }
+    public void setResources(String resources) { this.resources = resources; }
+
+    public String getDifficulty() { return difficulty; }
+    public void setDifficulty(String difficulty) { this.difficulty = difficulty; }
+
+    public Integer getLevel() { return level; }
+    public void setLevel(Integer level) { this.level = level; }
+
+    public Float getPositionX() { return positionX; }
+    public void setPositionX(Float positionX) { this.positionX = positionX; }
+
+    public Float getPositionY() { return positionY; }
+    public void setPositionY(Float positionY) { this.positionY = positionY; }
+
+    public Boolean getIsLocked() { return isLocked; }
+    public void setIsLocked(Boolean isLocked) { this.isLocked = isLocked; }
+
+    public Long getMapId() { return mapId; }
+    public void setMapId(Long mapId) { this.mapId = mapId; }
+}
