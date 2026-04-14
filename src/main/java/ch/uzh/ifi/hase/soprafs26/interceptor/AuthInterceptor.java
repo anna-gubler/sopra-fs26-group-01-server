fb4764a4c27bba3e3c12d838d381781683427642
sebdahub <sebastian.huber2@uzh.ch>
2026-04-14 14:48:28 +0200

    fix(#NA): handled authorization of CORS preflight requests with CorsMapping in WebConfig, removed corresponding hotfix in Interceptor

--- src/main/java/ch/uzh/ifi/hase/soprafs26/interceptor/AuthInterceptor.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/interceptor/AuthInterceptor.java
@@ -19,10 +19,6 @@ public class AuthInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-            return true;
-        }
-
         String authorizationHeader = request.getHeader("Authorization");
 
         if (authorizationHeader == null || authorizationHeader.isBlank()) {
