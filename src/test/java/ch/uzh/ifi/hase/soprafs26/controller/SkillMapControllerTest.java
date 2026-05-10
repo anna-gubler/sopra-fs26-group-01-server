cd0600942131230b8ec1e518e3e6168a2abee2d3
sebdahub <sebastian.huber2@uzh.ch>
2026-05-10 21:04:12 +0200

    Removed unused lines of code

--- src/test/java/ch/uzh/ifi/hase/soprafs26/controller/SkillMapControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs26/controller/SkillMapControllerTest.java
@@ -19,12 +19,9 @@ import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
